2330284d7f99d6032b3b75397da7f4e3607dafec
Oleg Kan <olegkahn@gmail.com>
2023-01-30 20:12:32 +0900

    my version 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+//        System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("catB");
+        Cat catBeanTwo =
+                (Cat) applicationContext.getBean("catB");
+
+        System.out.println("Is HelloWords equal? " + (bean == beanTwo));
+        System.out.println("Is Cats equal? " + (catBean == catBeanTwo));
     }
 }
